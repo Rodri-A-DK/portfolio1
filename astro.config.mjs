56bff8a30ef02cbec84c3686b51fa63d3df20dbf
unknown <rodrieldk1@gmail.com>
2025-01-02 01:38:11 -0300

    segundo comit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  base: '/portfolio1/', // Cambia al nombre exacto de tu repositorio
+  integrations: [tailwind()],
+  vite: {
+    build: {
+      outDir: 'build', // Carpeta de salida para la build
+    },
+  },
 });
